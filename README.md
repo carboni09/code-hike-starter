3c231e0df296695de35fa1b547d980457515a495
carboni09 <caleb.adegbite@hotmail.com>
2022-09-05 12:54:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-fp1kte
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-fp1kte)
---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '@code-hike/mdx/styles';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <article style={{ maxWidth: 768, margin: '0 auto', fontFamily: 'Inter' }}>
+      <Component {...pageProps} />
+    </article>
+  );
+}
+
+export default MyApp;
